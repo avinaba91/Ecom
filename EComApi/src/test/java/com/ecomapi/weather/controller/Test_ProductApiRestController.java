fa51bfbd89d7199e4e3bb433f2bb3e322b1505ef
Avinaba <avinaba91@gmail.com>
2018-09-28 03:24:22 +0530

    Initial changes

---
+++ EComApi/src/test/java/com/ecomapi/weather/controller/Test_ProductApiRestController.java
@@ -0,0 +1,14 @@
+package com.ecomapi.weather.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test_ProductApiRestController {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
